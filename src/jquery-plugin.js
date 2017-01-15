6a45d636e424047873fae38e7f42f528c5a3391b
aeei <khm@imtsoft.org>
2017-01-15 18:12:46 +0900

    delete same

--- src/jquery-plugin.js
+++ src/jquery-plugin.js
@@ -8,12 +8,13 @@
                 console.log(msg);
             }
 
-            function toArray(args){
+            function toArray(args) {
                 return [].slice.call(args);
             }
 
             /**
-             * 넘어온 인자의 모든 값을 비교(동등연산자 "==" 사용) 인자가 2개 미만일 때, false 반환
+             * 원시 값 비교 (string, number, boolean, null, undefined)
+             * 넘어온 인자의 모든 값을 비교, 인자가 2개 미만일 때, false 반환
              * @returns {Boolean}
              */
             function equal() {
@@ -25,40 +26,22 @@
                     var arr = toArray(arguments);
                     var len = arr.length;
 
-                    while(--len) {
-                        if (arr[len] != arr[len - 1]) rtn++;
+                    while (--len) {
+                        // TODO: Compare object
+                        if (arr[len] instanceof Object && arr[len - 1] instanceof Object) {
+                            if (JSON.stringify(arr[len]) !== JSON.stringify(arr[len - 1])) rtn++;
+                        } else {
+                            if (arr[len] != arr[len - 1]) rtn++;
+                        }
                     }
 
                     return !rtn;
                 }
             }
-
-            /**
-             * 넘어온 인자의 모든 값을 비교(일치연산자 "===" 사용) 인자가 2개 미만일 때, false 반환
-             * @returns {Boolean}
-             */
-            function same() {
-                if (arguments.length < 2) {
-                    log("The length of the arguments cannot be less than 2."); // 구글 번역기 POWER!
-                    return false;
-                } else {
-                    var rtn = 0;
-                    var arr = toArray(arguments);
-                    var len = arr.length;
-
-                    while(--len) {
-                        if (arr[len] !== arr[len - 1]) rtn++;
-                    }
-
-                    return !rtn;
-                }
-            }
-
             return {
                 log: log,
                 toArray: toArray,
-                equal: equal,
-                same: same
+                equal: equal
             };
         })() // close IIFE
     }); // close $.extend
