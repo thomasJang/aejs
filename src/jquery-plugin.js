6078b6932e8a91de7105dce095a7c90f66f13935
aeei <khm@imtsoft.org>
2017-01-16 21:24:26 +0900

    update

--- src/jquery-plugin.js
+++ src/jquery-plugin.js
@@ -1,50 +1,92 @@
-(function ($) {
-    $.extend($, {
-        aeei: (function () {
-            /**
-             * @param {String} msg
-             */
-            function log(msg) {
-                console.log(msg);
-            }
-            /**
-             * @param {*...} args
-             * @returns {Array}
-             */
-            function toArray(args) {
-                return [].slice.call(args);
-            }
-            /**
-             * 원시 값 비교 (string, number, boolean, null, undefined)
-             * 넘어온 인자의 모든 값을 비교, 인자가 2개 미만일 때, false 반환
-             * @returns {Boolean}
-             */
-            function equal() {
-                if (arguments.length < 2) {
-                    log("The length of the arguments cannot be less than 2."); // 구글 번역기 POWER!
-                    return false;
-                } else {
-                    var rtn = 0;
-                    var arr = toArray(arguments);
-                    var len = arr.length;
+(function () {
+    var root = this;
+    root["ae"] = (function () {
+        // 원시 값 클래스
+        const PV = ["[object String]", "[object Number]", "[object Boolean]", "[object Null]", "[object Undefined]"];
+        /**
+         * console log
+         * @param {String} msg
+         */
+        var log = function (msg) {
+            console.log(msg);
+        };
+        /**
+         * console log
+         * @param {String} msg
+         */
+        var error = function (msg) {
+            throw new Error(msg);
+        };
+        /**
+         * detect object class
+         * @param {Object} obj
+         * @returns {String}
+         */
+        var toString = function (obj) {
+            ({}).toString.call(obj);
+        };
+        /**
+         * 객체의 클래스가 원시 값인지 찬반형 결과를 반환
+         * @param obj
+         * @returns {boolean}
+         */
+        var isPrimitive = function (obj) {
+            return PV.indexOf(toString(obj)) > -1;
+        }
+        var isArray = function (obj) {
+            return toString(obj) === "[object Array]";
+        }
+        var isObject = function (obj) {
+            return toString(obj) === "[object Object]";
+        }
 
-                    while (--len) {
-                        // TODO: Compare object
-                        if (arr[len] instanceof Object && arr[len - 1] instanceof Object) {
-                            if (JSON.stringify(arr[len]) !== JSON.stringify(arr[len - 1])) rtn++;
-                        } else {
-                            if (arr[len] != arr[len - 1]) rtn++;
-                        }
+        /**
+         * 원시 값 비교 (string, number, boolean, null, undefined)
+         * 넘어온 인자의 모든 값을 비교, 인자가 2개 미만일 때, false 반환
+         * @returns {Boolean}
+         */
+        function equalAll() {
+            if (arguments.length < 2) {
+                log("The length of the arguments cannot be less than 2."); // 구글 번역기 POWER!
+                return false;
+            } else {
+                var rtn = 0;
+                var arr = [].slice.call(arguments);
+                var len = arr.length;
+
+                while (--len) { // arr.length - 1 만큼 비교
+                    // TODO: Compare object
+                    if (!isPrimitive[arr[len]] && !isPrimitive[arr[len - 1]]) {
+                        if (JSON.stringify(arr[len]) !== JSON.stringify(arr[len - 1])) rtn++;
+                    } else {
+                        if (arr[len] != arr[len - 1]) rtn++;
                     }
-
-                    return !rtn;
                 }
+                return !rtn;
             }
-            return {
-                log: log,
-                toArray: toArray,
-                equal: equal
-            };
-        })() // close IIFE
-    }); // close $.extend
-})(jQuery);
+        }
+        /**
+         * 원시 값 비교 (string, number, boolean, null, undefined)
+         * 넘어온 인자의 모든 값을 비교, 인자가 2개 미만일 때, false 반환
+         * 비교 대상을 첫 번째 인자로 설정!
+         * @returns {String}
+         */
+        function equalAllRtnMsg() {
+            if (arguments.length < 2) {
+                return "The length of the arguments cannot be less than 2.";
+            } else {
+                var rtn = [];
+                var arr = [].slice.call(arguments);
+                var len = arr.length;
+
+                while (--len) { // arr.length - 1 만큼 비교
+                    if (arr[0] !== arr[arr.length - len]) rtn.push("arg[" + (arr.length - len) + "]");
+                }
+                return rtn.join(', ') ? rtn.join(', ') + " do not match " + arr[0] : "" ;
+            }
+        }
+        return {
+            equalAll: equalAllRtnMsg
+        };
+    })(); // end IIFE ae
+}).call(this);
--- test/test.js
+++ test/test.js
@@ -1,43 +1,23 @@
-describe('Test equal : compare all primitive value', function(){
+describe('Test equal : compare all primitive value', function () {
     'use strict';
 
-    it('Test number', function(done){
-        if ($.aeei.equal(11, 11) && !$.aeei.equal(11, 21)) {
-            done();
-        } else {
-            throw Error('Error : Test number');
-        }
+    it('Test number', function (done) {
+        done(ae.equalAll(11, 11));
     });
 
-    it('Test string', function(done){
-        if ($.aeei.equal("11", '11') && !$.aeei.equal("11", "1 1")) {
-            done();
-        } else {
-            throw Error('Error : Test string');
-        }
+    it('Test string', function (done) {
+        done(ae.equalAll("11", '11'));
     });
 
-    it('Test boolean', function(done){
-        if ($.aeei.equal(true, true) && !$.aeei.equal(true, false)) {
-            done();
-        } else {
-            throw Error('Error : Test boolean');
-        }
+    it('Test boolean', function (done) {
+        done(ae.equalAll(true, true));
     });
 
-    it('Test null', function(done){
-        if ($.aeei.equal(null, null) && !$.aeei.equal(null, 'null')) {
-            done();
-        } else {
-            throw Error('Error : Test null');
-        }
+    it('Test null', function (done) {
+        done(ae.equalAll(null, null));
     });
 
-    it('Test undefined', function(done){
-        if ($.aeei.equal(undefined, undefined) && !$.aeei.equal(undefined, 'undefined')) {
-            done();
-        } else {
-            throw Error('Error : Test undefined');
-        }
+    it('Test undefined', function (done) {
+        done(ae.equalAll(undefined, undefined));
     });
 });
