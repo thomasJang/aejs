b8b5eb98717bfd5cd8539df99ae2fa2612b506a4
aeei <khm@imtsoft.org>
2017-01-26 22:26:47 +0900

    refactoring

--- src/ae.js
+++ src/ae.js
@@ -11,6 +11,7 @@
         ];
         const RETURN_MESSAGES = {
             "LENGTH": "Key length mismatch.",
+            "KEY": "Key mismatch.",
             "CLASS": "Class type mismatch.",
             "VALUE": "Value mismatch."
         };
@@ -46,20 +47,12 @@
         var isObject = function (obj) {
             return toString(obj) === "[object Object]";
         };
-        var enQ = function (a, b, Q) {
-            Q.push(JSON.stringify(a) + ", " + JSON.stringify(b));
-            console.log(Q[Q.length - 1]);
-            return Q[0];
-        };
 
         var isEqual = function (a, b) {
-            var stackA = []; // 비교할 a값이 저장될 스택
-            var stackB = []; // 비교할 b값이 저장될 스택
+            var stackA = [a]; // 비교할 a값이 저장될 스택
+            var stackB = [b]; // 비교할 b값이 저장될 스택
             var currA;  // stackA의 TOP
             var currB;  // stackB의 TOP
-
-            stackA.push(a);
-            stackB.push(b);
 
             while (stackA.length) {
                 currA = stackA.pop();
@@ -102,7 +95,7 @@
                             }
                         }// end isObject
                     }// end else
-                }
+                }//
             }// end while
 
             stackA = null;
@@ -112,7 +105,70 @@
             return true;
         };
 
-        var isEqualRecusive = function (a, b) {
+        var equal = function(a, b) {
+            var stackA = [a]; // 비교할 a값이 저장될 스택
+            var stackB = [b]; // 비교할 b값이 저장될 스택
+            var currA;  // stackA의 TOP
+            var currB;  // stackB의 TOP
+
+            var message = function(msg) {
+            return JSON.stringify(currA) + " and " + JSON.stringify(currB) + " " + RETURN_MESSAGES[msg];
+            };
+
+            while (stackA.length) {
+                currA = stackA.pop();
+                currB = stackB.pop();
+
+                // 클래스 타입이 다를 경우 false 반환
+                var str = toString(currA);
+                if (str !== toString(currB)) return message("CLASS");
+
+                // 비교 값들이 원시 값일 경우 비교 후 결과 반환
+                if (isContains(str, PRIMITIVE_VALUES)){
+                    if (currA !== currB) return message("VALUE");
+                }
+                // 원시 값이 아닌 다른 클래스를 가질 경우
+                else {
+                    // 오브젝트 키 길이를 비교 (Array, Object)
+                    var len = keys(currA).length;
+                    if (len !== keys(currB).length) {
+                        return message("LENGTH"); // 키 길이가 다를 경우 false 반환
+                    }
+                    else {
+                        // 비교 값이 배열일 경우
+                        if (str === "[object Array]") {
+                            while (len--) {
+                                stackA.push(currA[len]);
+                                stackB.push(currB[len]);
+                            }
+                        }// end isArray
+                        // 비교 값이 배열 외의 오브젝트일 경우 "[object Object]"
+                        else {
+                            while (len--) {
+                                var key = keys(currA)[len];
+                                if (currB.hasOwnProperty(key)) {
+                                    stackA.push(currA[key]);
+                                    stackB.push(currB[key]);
+                                }
+                                else {
+                                    return message("KEY");
+                                }
+                            }
+                        }// end isObject
+                    }// end else
+                }//
+            }// end while
+
+            stackA = null;
+            stackB = null;
+            currA = null;
+            currB = null;
+            message = null;
+
+            return "";
+        };
+
+        /*var recursive = function (a, b) {
             // 클래스를 비교, 클래스 문자열 저장
             var str = toString(a);
             if (str !== toString(b)) return false;
@@ -128,45 +184,17 @@
             // 비교 값이 배열일 경우
             if (str === "[object Array]") {
                 while (len--) {
-                    if (!isEqualRecusive(a[len], b[len])) return false;
+                    if (!recursive(a[len], b[len])) return false;
                 }
                 // 비교 값이 배열 외의 오브젝트일 경우 "[object Object]"
             } else {
                 while (len--) {
                     var key = keys(a)[len];
-                    if (!(b.hasOwnProperty(key) && isEqualRecusive(a[key], b[key]))) return false;
+                    if (!(b.hasOwnProperty(key) && recursive(a[key], b[key]))) return false;
                 }
             }
             return true;
-        };
-
-        var equal = function (a, b, Q) {
-            Q = Q || [];
-            // 클래스를 비교, 클래스 문자열 저장
-            var str = toString(a);
-            if (str !== toString(b)) return enQ(a, b, Q);
-
-            // 비교 값들이 원시 값일 경우 비교 후 결과 반환
-            if (isContains(str, PRIMITIVE_VALUES)) return a === b ? "" : enQ(a, b, Q);
-
-            // 원시 값이 아닌 다른 클래스를 가질 경우 오브젝트 키 길이를 비교 (Array, Object)
-            var len = keys(a).length;
-            if (len !== keys(b).length) return enQ(a, b, Q);
-
-            // 비교 값이 배열일 경우
-            if (str === "[object Array]") {
-                while (len--) {
-                    if (equal(a[len], b[len], Q)) return enQ(a, b, Q);
-                }
-                // 비교 값이 배열 외의 오브젝트일 경우 "[object Object]"
-            } else {
-                while (len--) {
-                    var key = keys(a)[len];
-                    if (!(b.hasOwnProperty(key) && !equal(a[key], b[key], Q))) return enQ(a, b, Q);
-                }
-            }
-            return "";
-        };
+        };*/
 
         function isEqualAll() {
             var arr = toArray(arguments);
@@ -175,6 +203,8 @@
             while (--len) {
                 if (!isEqual(arr[len], arr[len - 1])) return false;
             }
+            arr = null;
+            len = null;
             return true;
         }
 
@@ -186,6 +216,8 @@
             while (--len) {
                 if (msg = equal(arr[len], arr[len - 1])) return msg;
             }
+            arr = null;
+            len = null;
             return msg;
         }
 
